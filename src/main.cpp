673a3c541e2ff44f9f8a7d3531a16a44ca24f9e5
Victoria Wolter <daemoneye2@gmail.com>
2022-11-23 18:25:19 -0500

    Able to load equipment files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
+#include <filesystem>
 #include <iostream>
-#include "attack.h"
+#include <vector>
+#include "equipment.h"
 using namespace std;
+namespace fs = std::filesystem;
+
+void load_equipment(string directory, vector <equipment> *items);
 
 int main(int argc, char *argv[])
 {
+	vector <equipment> items;
+
+	string path = "/home/daemoneye/Documents/code/cpp/dnd/txt/equipment";
+	load_equipment(path, &items);
 	return 0;
+}
+
+void load_equipment(string directory, vector <equipment> *items)
+{
+	equipment tmp;
+	for (const auto & entry : fs::directory_iterator(directory))
+	{
+		tmp.load(entry.path());
+		cout << "[+] Loaded " << tmp.get_name() << endl;
+		items->push_back(tmp);
+	}
 }
