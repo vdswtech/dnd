9c2c1473cead95a124a8d5631ff36b4af183f14b
Victoria Wolter <daemoneye2@gmail.com>
2022-12-18 18:58:41 -0500

    Add languages table

--- setup.bash
+++ setup.bash
@@ -7,3 +7,26 @@ then
 else
 	echo "Found sqlite3." >&2
 fi
+
+sqlite3 $HOME/.dnd.db "create table languages (name TEXT);"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Common');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Dwarvish');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Elvish');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Giant');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Gnomish');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Goblin');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Halfling');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Orc');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Abyssal');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Celestial');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Draconic');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Deep Speech');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Infernal');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Primordial');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Sylvan');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Undercommon');"
+sqlite3 $HOME/.dnd.db "insert into languages (name) values ('Druidic');"
+
+echo "Languages"
+echo "========="
+sqlite3 $HOME/.dnd.db "select * from languages;"
